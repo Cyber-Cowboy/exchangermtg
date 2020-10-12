ee1781b054b58a58f6752e66e2d5ea38136aecd1
Cyber-Cowboy <OlegKorkeshkoV@yandex.ru>
2020-10-12 11:42:36 +0500

    models added

---
+++ plist/models.py
@@ -0,0 +1,30 @@
+import json
+import requests
+from django.db import models
+from django.contrib.auth.models import User
+
+class CardList(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return "%s's cardlist" % self.user.username
+
+class Card(models.Model):
+	name = models.CharField(max_length=100)
+	mset = models.CharField(max_length=5)
+	foil = models.BooleanField(default=False)
+	alt_art = models.BooleanField(default=False)
+	my_price = models.IntegerField()
+	card_list = models.ForeignKey(CardList, on_delete=models.CASCADE)
+	condition = models.CharField(max_length=2, choices=[("M","M"),
+												("NM","NM"),
+												("SP","SP"),
+												("HP","HP")], default="NM")
+
+	def __str__(self):
+		return self.name
+	
+	def get_market_price(self):
+		response = requests.get("https://api.scryfall.com/cards/named?exact=%s")%self.name
+		return json.loads(response)["prices"]["usd"]
+
